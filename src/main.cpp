8936c739292b31273452e04ef6f2b3511a788b44
Ivan Poroskun <ivan.poroskun@ptb.de>
2022-11-15 16:37:18 +0100

    Rename project to longman

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 // This file will be generated automatically when you run the CMake configuration step.
-// It creates a namespace called `myproject`.
 // You can modify the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
 #include <fmt/format.h>
 
+namespace longman = iporoskun::longman;
 
 // NOLINTNEXTLINE(bugprone-exception-escape)
 int main()
 {
-  fmt::print("{}\n", myproject::cmake::project_version);
+  fmt::print("{}\n", longman::cmake::project_version);
   return EXIT_SUCCESS;
 }

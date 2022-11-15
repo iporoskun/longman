a7edd13c78d36ea4578504a0e08ec34bcd19a89a
Ivan Poroskun <ivan.poroskun@ptb.de>
2022-11-15 18:17:24 +0100

    Update clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-// This file will be generated automatically when you run the CMake configuration step.
-// You can modify the source template at `configured_files/config.hpp.in`.
+// This file will be generated automatically when you run the CMake
+// configuration step. You can modify the source template at
+// `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
 // #include <fmt/format.h>
@@ -7,8 +8,7 @@
 namespace longman = iporoskun::longman;
 
 // NOLINTNEXTLINE(bugprone-exception-escape)
-int main()
-{
+int main() {
   // fmt::print("{}\n", longman::cmake::project_version);
   return EXIT_SUCCESS;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,13 +1,12 @@
 #include <catch2/catch.hpp>
 #include <iporoskun/longman.hpp>
 
-unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
+unsigned int Factorial(unsigned int number) // NOLINT(misc-no-recursion)
 {
   return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
-TEST_CASE("Factorials are computed", "[factorial]")
-{
+TEST_CASE("Factorials are computed", "[factorial]") {
   REQUIRE(Factorial(1) == 1);
   REQUIRE(Factorial(2) == 2);
   REQUIRE(Factorial(3) == 6);

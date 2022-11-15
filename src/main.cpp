a32c9c6ce24d4003213214b13c31985679748c99
Ivan Poroskun <ivan.poroskun@ptb.de>
2022-11-15 18:06:12 +0100

    Add longman static library

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 // You can modify the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
-#include <fmt/format.h>
+// #include <fmt/format.h>
 
 namespace longman = iporoskun::longman;
 
 // NOLINTNEXTLINE(bugprone-exception-escape)
 int main()
 {
-  fmt::print("{}\n", longman::cmake::project_version);
+  // fmt::print("{}\n", longman::cmake::project_version);
   return EXIT_SUCCESS;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch.hpp>
+#include <iporoskun/longman.hpp>
 
 unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
 {

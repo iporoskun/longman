220a343af5571c5d88ff2bf062e1893a43a91414
Ivan Poroskun <ivan.poroskun@ptb.de>
2022-11-22 12:36:25 +0100

    Rename constants to constant

--- src/longman.cpp
+++ src/longman.cpp
@@ -3,7 +3,7 @@
 
 namespace iporoskun::longman {
 
-namespace constants {
+namespace constant {
 // Advances in Geophysical Methods Applied to Forensic Investigations
 // https://shorturl.at/azJ49
 constexpr floating_t a = 6.378270e8;
@@ -25,7 +25,7 @@ constexpr floating_t love_k2 = 0.303;
 constexpr floating_t beta = 1. + love_h2 - 3. / 2. * love_k2;
 
 constexpr floating_t rev_sec = 360. * 3600.;
-} // namespace constants
+} // namespace constant
 
 floating_t details::mean_longitude_moon(floating_t time) noexcept { // sm_rad
   // const auto sm_rad = details::dms_to_rad(270., 26., 14.72) +
@@ -35,7 +35,7 @@ floating_t details::mean_longitude_moon(floating_t time) noexcept { // sm_rad
 
   const auto sm_rad =
 	details::dms_to_rad(270., 26., 11.72)
-	+ details::deg_to_rad((1336. * constants::rev_sec + 1108406.05) / 3600.)
+	+ details::deg_to_rad((1336. * constant::rev_sec + 1108406.05) / 3600.)
 		* time
 	+ details::deg_to_rad(7.128 / 3600.) * std::pow(time, 2.)
 	+ details::deg_to_rad(0.0072 / 3600.) * std::pow(time, 3.);
@@ -52,7 +52,7 @@ floating_t
 
   const auto pm_rad =
 	details::dms_to_rad(334., 19., 46.42)
-	+ details::deg_to_rad((11. * constants::rev_sec + 392522.51) / 3600.) * time
+	+ details::deg_to_rad((11. * constant::rev_sec + 392522.51) / 3600.) * time
 	- details::deg_to_rad(37.15 / 3600.) * std::pow(time, 2.)
 	- details::deg_to_rad(0.036 / 3600.) * std::pow(time, 3.);
 
@@ -80,7 +80,7 @@ floating_t details::longitude_of_moons_ascending_node(
 
   const auto N_rad =
 	details::dms_to_rad(259., 10., 59.81)
-	- details::deg_to_rad((5. * constants::rev_sec + 482911.24) / 3600.) * time
+	- details::deg_to_rad((5. * constant::rev_sec + 482911.24) / 3600.) * time
 	+ details::deg_to_rad(7.48 / 3600.) * std::pow(time, 2.)
 	+ details::deg_to_rad(0.007 / 3600.) * std::pow(time, 3.);
   return N_rad;
@@ -118,7 +118,6 @@ void longman::calc_longitude_and_eccentricity(floating_t time) noexcept {
 auto longman::calculate_acceleration(const time_point& utc_time) const
   -> floating_t /* meters_per_second_squared_t*/
 {
-  using namespace constants;
   using fhours_t = std::chrono::duration<floating_t, std::ratio<3600>>;
   const auto t0 = details::from_midnight<fhours_t>(utc_time).count();
   const auto t_rad = details::deg_to_rad(
@@ -129,18 +128,19 @@ auto longman::calculate_acceleration(const time_point& utc_time) const
   const auto nu = longitude_celestial_equator();
 
   const auto cos_alpha =
-	cos(N_rad) * cos(nu) + sin(N_rad) * sin(nu) * cos(omega);
-  const auto sin_alpha = (sin(omega) * sin(N_rad)) / sin(Im_rad);
+	cos(N_rad) * cos(nu) + sin(N_rad) * sin(nu) * cos(constant::omega);
+  const auto sin_alpha = (sin(constant::omega) * sin(N_rad)) / sin(Im_rad);
   const auto alpha = 2 * atan(sin_alpha / (1. + cos_alpha));
 
   const auto xi = N_rad - alpha;
   const auto sigma = sm_rad - xi;
 
   const auto L_m_rad =
-	sigma + 2. * e_m * sin(sm_rad - pm_rad)
-	+ 5. / 4. * pow(e_m, 2.) * sin(2. * (sm_rad - pm_rad))
-	+ 15. / 4. * m_s2m * e_m * sin(sm_rad - 2. * hs_rad + pm_rad)
-	+ 11. / 8. * pow(m_s2m, 2.) * sin(2. * (sm_rad - hs_rad));
+	sigma + 2. * constant::e_m * sin(sm_rad - pm_rad)
+	+ 5. / 4. * pow(constant::e_m, 2.) * sin(2. * (sm_rad - pm_rad))
+	+ 15. / 4. * constant::m_s2m * constant::e_m
+		* sin(sm_rad - 2. * hs_rad + pm_rad)
+	+ 11. / 8. * pow(constant::m_s2m, 2.) * sin(2. * (sm_rad - hs_rad));
 
   const auto L_sun_rad = hs_rad + 2. * es * sin(hs_rad - ps_rad);
 
@@ -157,19 +157,21 @@ auto longman::calculate_acceleration(const time_point& utc_time) const
   // vernal equinox
   const auto chi_s_rad = t_rad + hs_rad;
   const auto cos_Zs =
-	sin(pos_rad_cm.latitude) * sin(omega) * sin(L_sun_rad)
+	sin(pos_rad_cm.latitude) * sin(constant::omega) * sin(L_sun_rad)
 	+ cos(pos_rad_cm.latitude)
-		* (pow(cos(omega / 2.), 2.) * cos(L_sun_rad - chi_s_rad) + pow(sin(omega / 2.), 2.) * cos(L_sun_rad + chi_s_rad));
+		* (pow(cos(constant::omega / 2.), 2.) * cos(L_sun_rad - chi_s_rad) + pow(sin(constant::omega / 2.), 2.) * cos(L_sun_rad + chi_s_rad));
 
   // vertical component of tidal acceleration due to the Moon
-  const auto gm = G * M_m * r * pow(1. / d, 3.) * (3. * pow(cos_Zm, 2.) - 1.)
-				  + 1.5 * G * M_m * pow(r, 2.) * pow(1. / d, 4.)
-					  * (5. * pow(cos_Zm, 3.) - 3. * cos_Zm);
+  const auto gm = constant::G * constant::M_m * r * pow(1. / d, 3.)
+					* (3. * pow(cos_Zm, 2.) - 1.)
+				  + 1.5 * constant::G * constant::M_m * pow(r, 2.)
+					  * pow(1. / d, 4.) * (5. * pow(cos_Zm, 3.) - 3. * cos_Zm);
 
   // vertical component of tidal acceleration due to the Sun :
-  const auto gs = G * M_s * r * pow(1. / D, 3.) * (3. * pow(cos_Zs, 2.) - 1.);
+  const auto gs = constant::G * constant::M_s * r * pow(1. / D, 3.)
+				  * (3. * pow(cos_Zs, 2.) - 1.);
 
-  const auto g0_gal = (gm + gs) * beta;
+  const auto g0_gal = (gm + gs) * constant::beta;
   // return { g0_gal * 1000., g0_gal / 100. };
   return /*meters_per_second_squared_t*/ floating_t{ g0_gal / 100. };
 }
@@ -178,13 +180,13 @@ auto longman::calculate_acceleration(const time_point& utc_time) const
 floating_t
   longman::distance_to_earth_centre(const position& pos_rad_cm) noexcept { // r
   const auto C_2 =
-	1. / (1. + constants::e_crt_2 * pow(sin(pos_rad_cm.latitude.get()), 2.));
+	1. / (1. + constant::e_crt_2 * pow(sin(pos_rad_cm.latitude.get()), 2.));
   const auto C = sqrt(C_2);
-  return C * constants::a + pos_rad_cm.height.get();
+  return C * constant::a + pos_rad_cm.height.get();
 }
 
 floating_t longman::distance_center_moon_earth() const noexcept { // d
-  using namespace constants;
+  using namespace constant;
   const auto a_moon = 1. / (c_m * (1. - pow(e_m, 2.)));
   const auto recip_d =
 	1. / c_m + a_moon * e_m * cos(sm_rad - pm_rad)
@@ -195,14 +197,14 @@ floating_t longman::distance_center_moon_earth() const noexcept { // d
 }
 
 floating_t longman::distance_center_sun_earth() const noexcept { // D
-  using namespace constants;
+  using namespace constant;
   const auto a_sun = 1. / (c_s * (1. - pow(es, 2.)));
   const auto recip_D = ((1. / c_s) + a_sun * es * cos(hs_rad - ps_rad));
   return static_cast<floating_t>(1) / recip_D;
 }
 
 floating_t longman::inclination_of_moon() const { // Im_rad
-  using namespace constants;
+  using namespace constant;
   const auto inc_moon_rad =
 	acos(cos(omega) * cos(i) - sin(omega) * sin(i) * cos(N_rad));
 #if not defined(IPOROSKUN_LONGMAN_DISABLE_RUNTIME_CHECKS)
@@ -217,7 +219,7 @@ floating_t longman::inclination_of_moon() const { // Im_rad
 }
 
 floating_t longman::longitude_celestial_equator() const { // nu
-  const auto nu_t = asin(sin(constants::i) * sin(N_rad) / sin(Im_rad));
+  const auto nu_t = asin(sin(constant::i) * sin(N_rad) / sin(Im_rad));
 #if not defined(IPOROSKUN_LONGMAN_DISABLE_RUNTIME_CHECKS)
   if (
 	((nu_t * 180 / std::numbers::pi_v<floating_t>) < -15)
